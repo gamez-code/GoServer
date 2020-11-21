a7b48b69573569483e524b4357bda02a8f4c028e
Manuel Gamez Jimenez <manueljime@gmail.com>
2020-11-21 23:38:24 +0000

    Added multiplemiddleware

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main(){
 	server := NewServer(":3000")
 	server.Handle("/", HandleRoot)
-	server.Handle("/Home", server.AddMiddleware(HandleHome, CheckAuth()))
+	server.Handle("/Home", server.AddMiddleware(HandleHome, CheckAuth(), Logging()))
 	server.Listen()
 }
--- middleware.go
+++ middleware.go
@@ -3,18 +3,32 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"time"
+	"log"
 )
 
 func CheckAuth() Middleware {
 	return func(f http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request){
-			flag := false
+			flag := true
 			fmt.Println("Checking authentication")
 			if flag {
 				f(w, r)
 			} else {
 				return
 			}
+		}
+	}
+}
+
+func Logging() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request){
+			start := time.Now()
+			defer func() {
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			f(w, r)
 		}
 	}
 }
